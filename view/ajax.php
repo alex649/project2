aacd9582e88b99ea11244e9b75f3b64e97eb7e4b
alex649 <alexalex649@gmail.com>
2014-03-05 15:48:59 -0500

    Second Commit

--- view/ajax.php
+++ view/ajax.php
@@ -2,17 +2,8 @@
 
 /**
  * ajax.html
- * AJAX section example by Alain Ibrahim 
-*/
-
-/*
-error_reporting(0);
-
-
-if ($_POST["i_want_text"]) {
-    print "text received with value of " . $_POST["i_want_text"];
-   
-}
+ * 
+ * by Alex Spivakovsky
 */
 
 require_once('../model/model.php');
